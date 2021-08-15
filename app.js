15713fe38018e20e284c6b5d9a55b5ff15710956
Lucidreline <casta.ma502@gmail.com>
2021-08-14 20:58:11 -0700

    Delete Google Analytics.

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 let banner =
-  '\n\n' +
-  ' ██▓     █    ██  ▄████▄   ██▓▓█████▄  ██▀███  ▓█████  ██▓     ██▓ ███▄    █ ▓█████ \n' +
-  '▓██▒     ██  ▓██▒▒██▀ ▀█  ▓██▒▒██▀ ██▌▓██ ▒ ██▒▓█   ▀ ▓██▒    ▓██▒ ██ ▀█   █ ▓█   ▀ \n' +
-  '▒██░    ▓██  ▒██░▒▓█    ▄ ▒██▒░██   █▌▓██ ░▄█ ▒▒███   ▒██░    ▒██▒▓██  ▀█ ██▒▒███   \n' +
-  '▒██░    ▓▓█  ░██░▒▓▓▄ ▄██▒░██░░▓█▄   ▌▒██▀▀█▄  ▒▓█  ▄ ▒██░    ░██░▓██▒  ▐▌██▒▒▓█  ▄ \n' +
-  '░██████▒▒▒█████▓ ▒ ▓███▀ ░░██░░▒████▓ ░██▓ ▒██▒░▒████▒░██████▒░██░▒██░   ▓██░░▒████▒\n' +
-  '░ ▒░▓  ░░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░░▓   ▒▒▓  ▒ ░ ▒▓ ░▒▓░░░ ▒░ ░░ ▒░▓  ░░▓  ░ ▒░   ▒ ▒ ░░ ▒░ ░\n' +
-  '░ ░ ▒  ░░░▒░ ░ ░   ░  ▒    ▒ ░ ░ ▒  ▒   ░▒ ░ ▒░ ░ ░  ░░ ░ ▒  ░ ▒ ░░ ░░   ░ ▒░ ░ ░  ░\n' +
-  '  ░ ░    ░░░ ░ ░ ░         ▒ ░ ░ ░  ░   ░░   ░    ░     ░ ░    ▒ ░   ░   ░ ░    ░   \n' +
-  '    ░  ░   ░     ░ ░       ░     ░       ░        ░  ░    ░  ░ ░           ░    ░  ░\n' +
-  '                 ░             ░                                                    \n\n';
+  "\n\n" +
+  " ██▓     █    ██  ▄████▄   ██▓▓█████▄  ██▀███  ▓█████  ██▓     ██▓ ███▄    █ ▓█████ \n" +
+  "▓██▒     ██  ▓██▒▒██▀ ▀█  ▓██▒▒██▀ ██▌▓██ ▒ ██▒▓█   ▀ ▓██▒    ▓██▒ ██ ▀█   █ ▓█   ▀ \n" +
+  "▒██░    ▓██  ▒██░▒▓█    ▄ ▒██▒░██   █▌▓██ ░▄█ ▒▒███   ▒██░    ▒██▒▓██  ▀█ ██▒▒███   \n" +
+  "▒██░    ▓▓█  ░██░▒▓▓▄ ▄██▒░██░░▓█▄   ▌▒██▀▀█▄  ▒▓█  ▄ ▒██░    ░██░▓██▒  ▐▌██▒▒▓█  ▄ \n" +
+  "░██████▒▒▒█████▓ ▒ ▓███▀ ░░██░░▒████▓ ░██▓ ▒██▒░▒████▒░██████▒░██░▒██░   ▓██░░▒████▒\n" +
+  "░ ▒░▓  ░░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░░▓   ▒▒▓  ▒ ░ ▒▓ ░▒▓░░░ ▒░ ░░ ▒░▓  ░░▓  ░ ▒░   ▒ ▒ ░░ ▒░ ░\n" +
+  "░ ░ ▒  ░░░▒░ ░ ░   ░  ▒    ▒ ░ ░ ▒  ▒   ░▒ ░ ▒░ ░ ░  ░░ ░ ▒  ░ ▒ ░░ ░░   ░ ▒░ ░ ░  ░\n" +
+  "  ░ ░    ░░░ ░ ░ ░         ▒ ░ ░ ░  ░   ░░   ░    ░     ░ ░    ▒ ░   ░   ░ ░    ░   \n" +
+  "    ░  ░   ░     ░ ░       ░     ░       ░        ░  ░    ░  ░ ░           ░    ░  ░\n" +
+  "                 ░             ░                                                    \n\n";
 
 //The app's dependencies... we are just requiring all of the dependancies
-var methodOverride = require('method-override'), //Allows us to send PUT and DELETE actions from our forms
-  bodyParser = require('body-parser'), //allows us to get information from the user's form
-  flash = require('connect-flash'), //Gives us access to success and error messages for the user
-  mongoose = require('mongoose'), // Lets us connect to our database
-  passport = require('passport'), //Lets us hash the user's passwords
-  express = require('express'), //Gives us access to almost everything here, all the routes and server listener
+var methodOverride = require("method-override"), //Allows us to send PUT and DELETE actions from our forms
+  bodyParser = require("body-parser"), //allows us to get information from the user's form
+  flash = require("connect-flash"), //Gives us access to success and error messages for the user
+  mongoose = require("mongoose"), // Lets us connect to our database
+  passport = require("passport"), //Lets us hash the user's passwords
+  express = require("express"), //Gives us access to almost everything here, all the routes and server listener
   app = express(); //Puts express into a variable called app which you can see everywhere
 
 //Allows us to use our environmental variables. These are stored in /.env
-require('dotenv').config();
+require("dotenv").config();
 
 //This allows a user to stay logged in until they sign out or leave
 // thus preventing the user from having to log in every time they switch pages.
 app.use(
-  require('express-session')({
+  require("express-session")({
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false,
@@ -40,17 +40,17 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log('Successfully connected to our DataBase Son!' + banner);
+    console.log("Successfully connected to our DataBase Son!" + banner);
   })
-  .catch(err => {
-    console.log('Database Error: ' + err.message);
+  .catch((err) => {
+    console.log("Database Error: " + err.message);
   });
 
 //Lets config this app.. jazz it up a bit you know?
-app.set('view engine', 'ejs'); //We have to tell the app to always render ejs files so that we dont have to keep putting .ejs after each file... Stay DRY amigos
-app.use(express.static('public')); //Tells the app where we are going to put assets like photos, stylesheets, scripts, etc.
+app.set("view engine", "ejs"); //We have to tell the app to always render ejs files so that we dont have to keep putting .ejs after each file... Stay DRY amigos
+app.use(express.static("public")); //Tells the app where we are going to put assets like photos, stylesheets, scripts, etc.
 app.use(bodyParser.urlencoded({ extended: true })); //Allows us to get information from forms
-app.use(methodOverride('_method')); //Allows us to be able to use PUT and DELETE routes
+app.use(methodOverride("_method")); //Allows us to be able to use PUT and DELETE routes
 
 //Allows my app to use passport. This makes it possible to have secure passwords
 app.use(passport.initialize());
@@ -58,7 +58,7 @@ app.use(passport.session());
 
 app.use(flash()); //Tells the app to use flash
 
-var User = require('./models/user'); //Gives this file access to the User model
+var User = require("./models/user"); //Gives this file access to the User model
 
 //These contribute to having a user logged in and access to the signed in user.
 //This is useful if we want to, for example, display the user's name on the nav bar
@@ -77,15 +77,13 @@ app.use(function (req, res, next) {
   //This lets us access the current user's information.
   //We can use it to display the current users name or courses
   res.locals.loggedInUser = req.user; //lets us use the req.user variable from any page
-  res.locals.analyticsCode = process.env.GoogleAnalytics;
-  res.locals.error = req.flash('error'); // Gives all pages access to the error flash
-  res.locals.success = req.flash('success'); // Gives all pages access to the success flash
+  res.locals.error = req.flash("error"); // Gives all pages access to the error flash
+  res.locals.success = req.flash("success"); // Gives all pages access to the success flash
   next(); //Starts the next middlewar in the route or if there isnt any more, it will call the call back function
 });
 
-var allRoutes = ['index', 'user', 'course', 'grade', 'category'];
-allRoutes.forEach(route => app.use(require('./routes/' + route))); //gets the route files that are refactored into different files
-
+var allRoutes = ["index", "user", "course", "grade", "category"];
+allRoutes.forEach((route) => app.use(require("./routes/" + route))); //gets the route files that are refactored into different files
 
 app.listen(process.env.PORT, () =>
   console.log(`SERVER ON! PORT ${process.env.PORT}`)
